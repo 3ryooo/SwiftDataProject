d4a8869184113f6159b7c80be68a5c59246a9c1a
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-09 08:07:45 +0900

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
