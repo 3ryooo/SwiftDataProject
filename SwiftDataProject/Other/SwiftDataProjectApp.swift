9647d63b0a3f374e126d3890f5ba9e79854e56d5
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-09 08:30:43 +0900

    finish: https://www.hackingwithswift.com/books/ios-swiftui/editing-swiftdata-model-objects

--- SwiftDataProject/Other/SwiftDataProjectApp.swift
+++ SwiftDataProject/Other/SwiftDataProjectApp.swift
@@ -4,12 +4,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct SwiftDataProjectApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: User.self)
         }
     }
 }
